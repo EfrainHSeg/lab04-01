964b891f52cbcdf47026d8a8f9edfc75d55f744c
EfrainHSeg <efrainhsgi@gmail.com>
2023-09-21 10:48:43 -0500

    Primer commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 8080;
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/clientes', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'clientes.html'));
+});
+
+app.get('/productos', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'productos.html'));
+});
+
+app.listen(port, () => {
+  console.log(`La aplicación está escuchando en el puerto ${port}`);
+});
